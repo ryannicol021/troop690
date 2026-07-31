f049ba400f8f77aafb9f9fc1410a04f2569eed65
ryannicol021 <ryannicol021@gmail.com>
2026-07-31 18:04:47 -0400

    Update site.js

--- js/site.js
+++ js/site.js
@@ -319,7 +319,7 @@ async function initEagleScouts(){
             if(eagle.suffix){
                 name+=" "+eagle.suffix;
             }
-            card.innerHTML=`<h3><span style="color:var(--red);">${eagle.number}.</span> ${name}</h3><p>Eagle Scout Class of ${eagle.year}</p>`;
+            card.innerHTML=`<h3><span style="color:var(--red);">${eagle.number}.</span><br>${name}</h3><p>Eagle Scout Class of ${eagle.year}</p>`;
             results.appendChild(card);
         });
     });
