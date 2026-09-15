666136096aa60560a9be2114a84da70f80989d37
ryannicol021 <ryannicol021@gmail.com>
2026-09-15 16:06:00 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React,{useEffect,useRef,useState} from 'react';
 import {createRoot} from 'react-dom/client';
 import {BrowserRouter,useNavigate,useLocation} from 'react-router-dom';
 import './styles.css';
