e45c07bdf088336db5c99472e6222535c8e5a26b
ryannicol021 <ryannicol021@gmail.com>
2026-09-17 14:39:15 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5433,7 +5433,7 @@ const formatStoragePercent=(
 <div className="storage-usage-label">
   <span>
     {storageError?
-      storageError:
+      'Unavailable':
       storageBytes===null?
         'Loading…':
         `${formatStorage(
